beea25f7384480010a567eecdeeb78476151f729
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-12-16 18:08:14 +0100

    feature: set up Redux

---
+++ frontend/store/slices/authSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface IAuthState {
+  isAuthenticated: boolean;
+  accessToken: string | null;
+  refreshToken: string | null;
+}
+
+const initialState: IAuthState = {
+  isAuthenticated: false,
+  accessToken: null,
+  refreshToken: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login(
+      state,
+      action: PayloadAction<{ accessToken: string; refreshToken: string }>
+    ) {
+      state.isAuthenticated = true;
+      state.accessToken = action.payload.accessToken;
+      state.refreshToken = action.payload.refreshToken;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+      state.accessToken = null;
+      state.refreshToken = null;
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
