6042dd897c5f3f608f13d5c21f94f2e2d9d5a9f6
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-12-18 17:35:51 +0100

    feature: refactor registerUser function to include field validation and added loginUser function for authentication

--- frontend/app/actions/auth.ts
+++ frontend/app/actions/auth.ts
@@ -1,22 +1,43 @@
 "use server";
 
-export async function registerUser(formData: FormData) {
+export async function registerUser(formData: FormData): Promise<string> {
   const username = formData.get("username");
   const email = formData.get("email");
   const password = formData.get("password");
+  if (!username || !email || !password) {
+    throw new Error("All fields are required.");
+  }
 
   const response = await fetch("http://127.0.0.1:8000/api/users/register/", {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      username,
-      email,
-      password,
-    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, email, password }),
   });
 
   if (!response.ok) {
     throw new Error("Registration failed. Please try again.");
   }
+
   return "Registration successful";
+}
+
+export async function loginUser(formData: FormData) {
+  const username = formData.get("username");
+  const password = formData.get("password");
+
+  const response = await fetch("http://127.0.0.1:8000/api/token/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Invalid credentials");
+  }
+  const data = response.json();
+  return data;
 }
