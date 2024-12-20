02a8710af6079553692506764079810e879c069d
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-12-20 16:26:40 +0100

    feature: add Recommendation model, serializer, and initial migration

--- backend/recommendations/models.py
+++ backend/recommendations/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from users.models import CustomUser
+class Recommendation(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="recommendations")
+    course_name = models.CharField(max_length=255)
 
-# Create your models here.
+    def __str__(self):
+        return f"{self.user.username} - {self.course_name}"
+
+
--- backend/recommendations/serializers.py
+++ backend/recommendations/serializers.py
@@ -1,2 +1,10 @@
 from rest_framework import serializers
+from .models import Recommendation
+
+class RecommendationSerialzer(serializers.ModelSerializer):
+    class Meta:
+        model = Recommendation
+        fields = ["id","user","course_name"]
+
+
 
