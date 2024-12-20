c6a4c63d16b99bf36cca8516886547303c042395
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-12-20 16:25:39 +0100

    feature: add recommendations API endpoint and refactor user registration view

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,17 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 from .serializers import UserRegistrationSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 
-class RegisterView(APIView):
-    def post(self, request):
-        serializer = UserRegistrationSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return  Response(serializer.errors, status=status.HTTP_400_BAD_REQUESTs)
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = UserRegistrationSerializer
+
+class LogoutView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self,request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 class ProfileView(APIView):
     permission_classes = [IsAuthenticated]
