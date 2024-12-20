95256cc74863eabe50a1de1f5abff9e4678cae19
Adrianm7777 <Adrianmaterka10@gmail.com>
2024-12-20 16:26:53 +0100

    feature: add recommendations API view and URL routing

--- backend/recommendations/views.py
+++ backend/recommendations/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Recommendation
+from .serializers import RecommendationSerialzer
 
-# Create your views here.
+class RecommendationView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        recommendations = Recommendation.objects.filter(user=request.user)
+        serializer = RecommendationSerialzer(recommendations, many =True)
+        return Response({"recommendations":serializer.data})
+
+    def post(self,request):
+        data = request.data
+        data["user"] = request.user.id
+        serializer = RecommendationSerialzer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors,status=400)
